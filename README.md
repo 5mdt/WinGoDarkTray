f337650502e6ceeb2e2d7d8f6dc4f6312bac12cb
Andrey Gonyaev <andrey@gonyaev.com>
2024-09-27 21:25:19 +0300

    Remove resource compilation

--- README.md
+++ README.md
@@ -2,10 +2,6 @@
 
 A simple app to toggle between light and dark mode on Windows directly from the system tray.
 
-### Prerequisites
-
-[MinGW](https://www.mingw-w64.org/) for Windows resources compilation.
-
 ### Compiling
 
 Run `build.bat` to compile.
--- build.bat
+++ build.bat
@@ -3,18 +3,11 @@ setlocal
 
 rem Define variables
 set APP_NAME=godark.exe
-set RESOURCES=resources.rc
 set ICON_FILE=icon.ico
-set RESOURCE_SYSO=resources.syso
 
 rem Clean previous builds
 echo Cleaning up...
 if exist %APP_NAME% del %APP_NAME%
-if exist %RESOURCE_SYSO% del %RESOURCE_SYSO%
-
-rem Compile the resources
-echo Compiling resources...
-windres %RESOURCES% -O coff -o %RESOURCE_SYSO%
 
 rem Set architecture
 set GOOS=windows
