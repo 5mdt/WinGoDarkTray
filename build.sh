c51af8fce34830ceb9b6f8e6145bee87875a4d8e
Vladimir nett00n Budylnikov <git@nett00n.org>
2025-03-17 12:44:58 +0400

    add icon add x32/x64 build

--- build.sh
+++ build.sh
@@ -2,19 +2,62 @@
 
 # Define variables
 APP_NAME="WinGoDarkTray"
+ICON_FILE="icon.ico"
+BUILD_DIR="./build"
 
-# Clean previous builds
-echo "Cleaning up..."
-if [ -f "$APP_NAME" ]; then
-  rm "$APP_NAME"
-fi
+# Function to check if rsrc is installed
+check_rsrc_installed() {
+  command -v rsrc >/dev/null 2>&1 || {
+    echo "rsrc not found, installing...";
+    go install github.com/akavel/rsrc@latest;
+  }
+}
 
-# Set architecture
-GOOS="windows"
-GOARCH="amd64"
+# Function to clean up old build artifacts
+clean_up() {
+  echo "Cleaning up old artifacts..."
+  find . -name "*.exe" -delete
+  find $BUILD_DIR -name "*.exe" -delete
+  find . -name "*.syso" -delete
+}
 
-# Build the Go application
-echo "Building the application..."
-GOOS=$GOOS GOARCH=$GOARCH go build -ldflags=-H=windowsgui -o "$APP_NAME".exe
+# Function to create build directory if it doesn't exist
+create_build_dir() {
+  echo "Ensuring the build directory exists..."
+  mkdir -p $BUILD_DIR
+}
 
-echo "Build complete."
+# Function to embed the icon into the Go application
+embed_icon() {
+  echo "Embedding the icon..."
+  rsrc -ico "$ICON_FILE"
+}
+
+# Function to build the Go application for a specific architecture
+build_app() {
+  local arch=$1
+  local arch_name=$2
+  echo "Building the application for $arch_name ($arch)..."
+  GOARCH=$arch GOOS="windows" go build -ldflags=-H=windowsgui -o "$BUILD_DIR/$APP_NAME-$arch_name.exe"
+}
+
+# Function to clean up resources
+remove_syso_files() {
+  echo "Removing .syso files..."
+  find . -name "*.syso" -delete
+}
+
+# Main build function
+build() {
+  clean_up
+  create_build_dir
+  check_rsrc_installed
+  embed_icon
+  build_app "amd64" "x64"
+  build_app "386" "x32"
+  remove_syso_files
+  echo "Build complete."
+}
+
+# Run the build process
+build
