c6b2589850bea721a33110e4515237a96d393d3e
Vladimir nett00n Budylnikov <git@nett00n.org>
2025-09-19 21:51:56 +0400

    lint improvements

--- app_stub.go
+++ app_stub.go
@@ -8,11 +8,22 @@ package main
 import "fmt"
 
 // App represents the application state (stub for non-Windows)
-type App struct{}
+type App struct {
+	toggleSystemItem  interface{}
+	toggleAppItem     interface{}
+	toggleWindowsItem interface{}
+	updateNowItem     interface{}
+	version           string
+}
 
 // NewApp creates a new App instance (stub for non-Windows)
-func NewApp() *App {
-	return &App{}
+func NewApp(version string) *App {
+	if version == "" {
+		version = "v0.0.0"
+	}
+	return &App{
+		version: version,
+	}
 }
 
 // Run starts the application (stub for non-Windows)
